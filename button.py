0d05bdb8daf1f547c0c3d544a51fb7f186097f19
Sergey <ereskosl@gmail.com>
2021-08-25 18:44:20 +0300

    edited

--- button.py
+++ button.py
@@ -1,5 +1,6 @@
 import pygame.font
 
+
 class Button:
     """Create button class"""
 
@@ -16,11 +17,11 @@ class Button:
         self.prep_msg(msg)
 
     def prep_msg(self, msg):
-        self.msg_img = self.font.render(msg, True, self.text_color, self.button_color)
+        self.msg_img = self.font.render(
+            msg, True, self.text_color, self.button_color)
         self.msg_img_rect = self.msg_img.get_rect()
         self.msg_img_rect.center = self.rect.center
 
     def draw_button(self):
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_img, self.msg_img_rect)
-                
