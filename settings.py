2a6522eab0d2b2c39d870370a45711b0ae8f2852
Sergey <ereskosl@gmail.com>
2021-08-25 11:44:48 +0300

    divided into static and dynamic

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """Class for game settings storage"""
 
     def __init__(self) -> None:
-        """Initialize game settings"""
+        """Initializes static game settings"""
         #resourses
         self.ship_img = 'images/ship.bmp'
         self.alien_img = 'images/alien.bmp'
@@ -11,7 +11,6 @@ class Settings:
         self.screen_height = 400
         self.bg_color = (25, 25, 112)
         #ship
-        self.ship_speed = 1
         self.ship_limit = 3
         #bullets
         self.bullet_speed = 5
@@ -20,7 +19,21 @@ class Settings:
         self.bullet_color = (255,0,0)
         self.bullets_allowed = 3
         #aliens
-        self.alien_speed = .2
-        self.alien_drop_speed = 50
+        self.alien_speed = 0.7
+        self.alien_drop_speed = 10
+        self.alien_direction = -1
+        #game temp
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1
+        self.bullet_speed = 3
+        self.alien_speed = 0.7
         self.alien_direction = -1
 
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
